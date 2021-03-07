d9ce053ce0fa637e70b21de865d3a3a026b38a88
Ekow Abaka Ainooson <jainooson@gmail.com>
2021-03-06 21:31:31 -0600

    Updated highlight.js Got plugin compatible with new foonoo events

--- HighlightPlugin.php
+++ HighlightPlugin.php
@@ -3,28 +3,28 @@
 namespace foonoo\plugins\contrib\highlight;
 
 use foonoo\Plugin;
-use foonoo\events\ContentOutputGenerated;
+use foonoo\events\ContentLayoutApplied;
 use foonoo\events\AssetPipelineReady;
 
 class HighlightPlugin extends Plugin {
 
     public function getEvents() {
         return [
-            ContentOutputGenerated::class => function(ContentOutputGenerated $event) { $this->injectCode($event); },
+            ContentLayoutApplied::class => function(ContentLayoutApplied $event) { $this->injectCode($event); },
             AssetPipelineReady::class => function(AssetPipelineReady $event) { $this->injectAssets($event); }
         ];
     }
 
     private function injectAssets(AssetPipelineReady $event) {
-        $event->getAssetPipeline()->addJavascript(__DIR__ . "/assets/highlight/highlight.pack.js");
-        $event->getAssetPipeline()->addStylesheet(__DIR__ . "/assets/highlight/styles/agate.css");
+        $event->getAssetPipeline()->addItem(__DIR__ . "/assets/highlight/highlight.pack.js", "js");
+        $event->getAssetPipeline()->addItem(__DIR__ . "/assets/highlight/styles/mono-blue.css", "css");
     }
 
-    private function injectCode(ContentOutputGenerated $event) {
+    private function injectCode(ContentLayoutApplied $event) {
         $dom = $event->getDOM();
         $xpath = new \DOMXPath($dom);
         $headTag = $xpath->query("//head")->item(0);
-        $scriptTag = $dom->createElement("script", "hljs.initHighlightingOnLoad();");
+        $scriptTag = $dom->createElement("script", "window.addEventListener('load', ()=>hljs.highlightAll())");
         $headTag->appendChild($scriptTag);
     }
 }
