141511951e3dabc0aadb000076bff2d9dac617ea
James Ainooson <jainooson@gmail.com>
2025-02-22 05:47:14 -0600

    Updating the highlight plugin to use the new DOM

--- HighlightPlugin.php
+++ HighlightPlugin.php
@@ -15,18 +15,21 @@ class HighlightPlugin extends Plugin {
         ];
     }
 
-    private function injectAssets(AssetPipelineReady $event) {
+    private function injectAssets(AssetPipelineReady $event): void
+    {
         $style = $this->getOption('style', 'mono-blue');
         $event->getAssetPipeline()->addItem(__DIR__ . "/assets/highlight/styles/{$style}.css", "css");
         $event->getAssetPipeline()->addItem(__DIR__ . "/assets/highlight/highlight.min.js", "js");
     }
 
-    private function injectCode(ContentLayoutApplied $event) {
-        $dom = $event->getDOM();
+    private function injectCode(ContentLayoutApplied $event): void
+    {
+        $dom = $event->getDocument();
         if($dom !== null) {
-            $xpath = new \DOMXPath($dom);
-            $headTag = $xpath->query("//head")->item(0);
-            $scriptTag = $dom->createElement("script", "window.addEventListener('load', ()=>hljs.highlightAll())");
+            $headTag = $dom->querySelector("head");
+            //@var \Dom\Element
+            $scriptTag = $dom->createElement("script");
+            $scriptTag->textContent = "window.addEventListener('load', ()=>hljs.highlightAll())";
             $headTag->appendChild($scriptTag);            
         }
     }
